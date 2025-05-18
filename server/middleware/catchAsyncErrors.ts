87737f0c4e9f1780fa82c0819758ec002d21894f
Rupam Das <rejisterjack@gmail.com>
2025-05-18 09:18:15 +0530

    cleanup

--- server/middleware/catchAsyncErrors.ts
+++
@@ -1,8 +0,0 @@
-import type { NextFunction, Request, Response } from "express"
-
-const CatchAsyncError =
-  (fn: any) => (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next)
-  }
-
-export default CatchAsyncError
